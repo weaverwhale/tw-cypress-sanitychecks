0996c7536d16808ef944e7a15a3bbb07c0e8d336
Mike Weaver <michael@triplewhale.com>
2023-07-14 13:54:45 -0400

    CRON

---
+++ netlify/functions/schedule-deploy.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+
+// https://app.netlify.com/sites/tw-status/configuration/deploys
+const BUILD_HOOK =
+  "https://api.netlify.com/build_hooks/64b0538251e4203508d154bb";
+
+// https://crontab.guru/every-1-hour
+const handler = schedule("0 * * * *", async () => {
+  await fetch(BUILD_HOOK, {
+    method: "POST",
+  }).then((response) => {
+    console.log("Build hook response:", response);
+  });
+
+  return {
+    statusCode: 200,
+  };
+});
+
+export { handler };
