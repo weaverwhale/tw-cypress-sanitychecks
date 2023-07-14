e680c0be0b66bea612fb64b02da476a86ef0b5dc
Mike Weaver <michael@triplewhale.com>
2023-07-14 16:32:36 -0400

    screenshots

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -25,11 +25,9 @@ import addContext from "mochawesome/addContext";
 const titleToFileName = (title) => title.replace(/[:\/]/g, "");
 
 Cypress.on("test:after:run", (test, runnable) => {
-  if (test.state === "failed") {
-    const filename = `${titleToFileName(
-      runnable.parent.title
-    )} -- ${titleToFileName(test.title)} (failed).png`;
-    addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`);
-    addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
-  }
+  const filename = `${titleToFileName(
+    runnable.parent.title
+  )} -- ${titleToFileName(test.title)} (${test.state}).png`;
+  addContext({ test }, `../screenshots/${Cypress.spec.name}/${filename}`);
+  addContext({ test }, `../videos/${Cypress.spec.name}.mp4`);
 });
