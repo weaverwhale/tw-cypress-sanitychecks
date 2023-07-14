3c4e020c244b626dc2b8859fa99034bcfc7022b9
Mike Weaver <michael@triplewhale.com>
2023-07-14 14:05:53 -0400

    better willy test

--- cypress/e2e/willy.cy.js
+++ cypress/e2e/willy.cy.js
@@ -12,11 +12,22 @@ before(() => {
   cy.clearCache();
 });
 
-describe("summary page", () => {
-  it("has a menu item", () => {
+function goToWilly() {
+  cy.get(".lighthouse-wrapper div > svg").click();
+  cy.waitForNetworkIdle(1000);
+}
+
+describe("Willy", () => {
+  it("has an input", () => {
     cy.login();
-    cy.get(".lighthouse-wrapper div > svg").click();
-    cy.waitForNetworkIdle(1000);
+    goToWilly();
     cy.get("#willy-input").should("be.visible");
+  });
+
+  it("tries to answer our question", () => {
+    cy.login();
+    goToWilly();
+    cy.get("#willy-input").type("How are you?{enter}");
+    cy.get('span[aria-label*="Avatar"]').should("be.visible");
   });
 });
